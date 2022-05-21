1165dc905e776fb43a66a6582c03e9d6ae169759
bacter00 <h3po4@protonmail.com>
2022-05-21 22:58:50 +0200

    Add first js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var computerMove;
+computerMove = 'kamień';
+
+var playerMove ;
+playerMove = 'papier' ;
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to '+ playerMove +', to wygrywasz!');
